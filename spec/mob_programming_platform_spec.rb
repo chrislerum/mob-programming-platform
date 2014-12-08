e0dc0b96bcc37aaaa83937daf669d388c3f20be6
Pat Maddox <pat@rubysteps.com>
2014-12-08 02:58:54 -0800

    create a new session [closes #7]

--- spec/mob_programming_platform_spec.rb
+++ spec/mob_programming_platform_spec.rb
@@ -1,11 +1,21 @@
 class MobProgrammingPlatform
   def available_sessions
-    []
+    @available_sessions ||= []
+  end
+
+  def create_session
+    available_sessions << :new_session
   end
 end
 
 describe "Mob programming platform" do
   it "lists available sessions" do
     expect(MobProgrammingPlatform.new.available_sessions).to be_empty
+  end
+
+  it "creates new sessions" do
+    platform = MobProgrammingPlatform.new
+    platform.create_session
+    expect(platform.available_sessions.size).to eq(1)
   end
 end
