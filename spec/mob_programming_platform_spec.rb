10c3dfc73272250c8121b38f47299ff2dd6bc55d
Pat Maddox <pat@rubysteps.com>
2014-12-08 02:32:56 -0800

    Show available sessions [closes #5]

--- spec/mob_programming_platform_spec.rb
+++ spec/mob_programming_platform_spec.rb
@@ -1,5 +1,15 @@
+class MobProgrammingPlatform
+  def available_sessions
+    []
+  end
+end
+
 describe "Mob programming platform" do
   it "has a passing test" do
     expect(1).to be_odd
+  end
+
+  it "lists available sessions" do
+    expect(MobProgrammingPlatform.new.available_sessions).to be_empty
   end
 end
